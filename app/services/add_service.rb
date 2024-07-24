d42b09a433bca939a4650fc208a3350ee7549e80
SandeshaHG <sandeshhg1997@gmail.com>
2024-07-24 16:01:13 +0530

    [ProdCode] Added new methods to help with delimiter support

--- app/services/add_service.rb
+++ app/services/add_service.rb
@@ -1,15 +1,23 @@
 class AddService
     def add(input_string)
         validate_input(input_string)
+        delimiter = nil
         return 0 if input_string.empty?
 
         if input_string.match?(/\A\d+\z/)
             input_string.to_i
         end
-         input_string.split(/[\n,]/).map(&:to_i).sum
+
+        input_string.split(fetch_delimiter(input_string)).map(&:to_i).sum
     end
 
     def validate_input(input_string)
         raise ArgumentError, 'Input must be a string' unless input_string.is_a?(String)
+    end
+
+    def fetch_delimiter(input_string)
+        unless input_string.start_with?("//")
+            return /[\n,]/
+        end
     end
 end
