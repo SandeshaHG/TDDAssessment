e2b637f1cd2aba684c022950c15942fa13041659
SandeshaHG <sandeshhg1997@gmail.com>
2024-07-24 14:34:18 +0530

    [UnitTest] Check if a method called add exists

--- test/add_service_test.rb
+++ test/add_service_test.rb
@@ -4,4 +4,9 @@ class AddServiceTest < ActiveSupport::TestCase
   test "should confirm AddService class exists" do
     assert defined?(AddService), "AddService class does not exist"
   end
+
+  test 'should respond to add method' do
+    @service = AddService.new
+    assert_respond_to @service, :add
+  end
 end
