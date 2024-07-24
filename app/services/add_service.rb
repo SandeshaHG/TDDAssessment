df4eb7e7127051705be7b859788b6da20fb8ef61
SandeshaHG <sandeshhg1997@gmail.com>
2024-07-24 15:45:51 +0530

    [ProdCode]Refactored, All test passed on

--- app/services/add_service.rb
+++ app/services/add_service.rb
@@ -1,14 +1,15 @@
 class AddService
     def add(input_string)
-        unless input_string.is_a?(String)
-            raise ArgumentError, 'Input must be a string'
-        end
+        validate_input(input_string)
         return 0 if input_string.empty?
 
         if input_string.match?(/\A\d+\z/)
-            return input_string.to_i
+            input_string.to_i
         end
-        
-        return input_string.split(/[\n,]/).map(&:to_i).sum
+         input_string.split(/[\n,]/).map(&:to_i).sum
+    end
+
+    def validate_input(input_string)
+        raise ArgumentError, 'Input must be a string' unless input_string.is_a?(String)
     end
 end
