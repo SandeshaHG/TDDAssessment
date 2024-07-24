fa646e779610ba96c214aafc9a381149a9894454
SandeshaHG <sandeshhg1997@gmail.com>
2024-07-24 14:27:55 +0530

    [UnitTest] check is Add service class exists

---
+++ test/add_service_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddServiceTest < ActiveSupport::TestCase
+  test "should confirm AddService class exists" do
+    assert defined?(AddService), "AddService class does not exist"
+  end
+end
