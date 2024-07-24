2d971781a3839967bf4be5f6f309777230a908ef
SandeshaHG <sandeshhg1997@gmail.com>
2024-07-24 14:37:22 +0530

    [UnitTest] Added test to check if method accepts string

--- test/add_service_test.rb
+++ test/add_service_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class AddServiceTest < ActiveSupport::TestCase
+  def setup
+    @service = AddService.new
+  end
+
   test "should confirm AddService class exists" do
     assert defined?(AddService), "AddService class does not exist"
   end
 
   test 'should respond to add method' do
-    @service = AddService.new
     assert_respond_to @service, :add
+  end
+
+  test 'add method should accept a string input' do
+    assert_nothing_raised do
+      @service.add("sample string")
+    end
   end
 end
